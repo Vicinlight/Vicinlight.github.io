8b60ddbdd60a8f1862ae1830572beaa16b12b545
Vicinlight <38333404+Vicinlight@users.noreply.github.com>
2020-05-27 18:02:56 +0300

    2.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-
 $(function () {
     $('a.cv_head_nav__link, .cv_header__btn').each(function () {
-        var th = this;
+        let th = this;
         // alert($(th).attr('href'));
         $(th).on('click', function (e) {
             e.preventDefault();
-            var id = $(th).attr('href'),
+            let id = $(th).attr('href'),
                 top = $(id).offset().top;
             $('body,html').animate({scrollTop: top}, 1200);
             return false;
